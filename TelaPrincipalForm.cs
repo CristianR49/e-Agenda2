fa349000ad6e9ca7519118ba935b0d9f71e75408
Cristian <cristian.rr99@gmail.com>
2023-05-17 10:05:45 -0300

    Add project files.

---
+++ TelaPrincipalForm.cs
@@ -0,0 +1,10 @@
+namespace e_Agenda
+{
+    public partial class TelaPrincipalForm : Form
+    {
+        public TelaPrincipalForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
